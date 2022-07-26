d74389b64a072d191a89f0f0fe6fdce054acf0df
munifnagi <46773316+MunifNagi@users.noreply.github.com>
2022-07-25 22:50:25 -0400

    User can select sorting order

--- SelectionSort.java
+++ SelectionSort.java
@@ -3,13 +3,13 @@
  * sorting algorithm.
  */
 class SelectionSort {
-    public static void sort(int[] array) {
+    public static void sort(int[] array, boolean desc) {
 
         for (int i = 0; i < array.length - 1; i++) {
             int index = i;
 
             for (int j = i + 1; j < array.length; j++) {// finds the minimum element in array[i..]
-                if (array[j] < array[index]) {
+                if ((!desc && (array[j] < array[index])) || (desc && (array[j] > array[index]))) {
                     index = j;
                 }
             }
@@ -30,11 +30,13 @@ class SelectionSort {
 
     public static void main(String[] args) {
         int[] array = { 1, 23, 3, 4, 2, 14, 10, 8, 20, 9, 21 };
-
         System.out.println("Array before sorting");
         printArray(array);
-        sort(array); // sort the array
-        System.out.println("Array after sorting");
+        System.out.println("Array sorted in ascending order");
+        sort(array, false); // sort the array in ascending order
+        printArray(array);
+        sort(array, true); // sort the array in descending order
+        System.out.println("Array sorted in descending order");
         printArray(array);
     }
 
