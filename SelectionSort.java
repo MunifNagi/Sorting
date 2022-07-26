e3b02faa761b0bf0802871553c9906ae66e479af
munifnagi <46773316+MunifNagi@users.noreply.github.com>
2022-07-26 02:43:28 -0400

    removing unecssary code, and other methods to main class

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,10 +1,7 @@
-/**
- * The SelectionSort class implements the selection
- * sorting algorithm.
- */
-class SelectionSort {
+public class SelectionSort {
     public static void sort(int[] array, boolean desc) {
-
+        System.out.println("Array before sorting");
+        printArray(array);
         for (int i = 0; i < array.length - 1; i++) {
             int index = i;
 
@@ -22,6 +19,7 @@ class SelectionSort {
     }
 
     public static void sort(char[] array, boolean desc) {
+        System.out.println("Array before sorting");
 
         for (int i = 0; i < array.length - 1; i++) {
             int index = i;
@@ -42,7 +40,6 @@ class SelectionSort {
     }
 
     public static void sort(String[] array, boolean desc) {
-
         for (int i = 0; i < array.length - 1; i++) {
             int index = i;
 
@@ -59,58 +56,6 @@ class SelectionSort {
 
         }
 
-    }
-
-    static void printArray(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println("");
-    }
-
-    static void printArray(char[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println("");
-    }
-
-    static void printArray(String[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println("");
-    }
-
-    public static void main(String[] args) {
-        int[] numArray = { 1, 23, 3, 4, 2, 14, 10, 8, 20, 9, 21 };
-        char[] charArray = { 'a', 'z', 'K', 'A', 'X', 'M', };
-        String[] stringArray = { "zc", "aba", "ab", "ba", "abc", "a", };
-
-        System.out.println("Arrays before sorting");
-        printArray(numArray);
-        printArray(charArray);
-        printArray(stringArray);
-
-        // sorting array in Ascending order
-        sort(numArray, false);
-        sort(charArray, false);
-        sort(stringArray, false);
-
-        System.out.println("Arrays sorted in ascending order");
-        printArray(numArray);
-        printArray(charArray);
-        printArray(stringArray);
-
-        // sorting array in Descending order
-        sort(numArray, true);
-        sort(charArray, true);
-        sort(stringArray, true);
-
-        System.out.println("Arrays sorted in descending order");
-        printArray(numArray);
-        printArray(charArray);
-        printArray(stringArray);
     }
 
 }
