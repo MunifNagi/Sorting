259420d884d519134ddb611426e849abb51959f1
munifnagi <46773316+MunifNagi@users.noreply.github.com>
2022-07-25 23:00:39 -0400

    User can sort numbers, chars, and strings

--- SelectionSort.java
+++ SelectionSort.java
@@ -21,6 +21,46 @@ class SelectionSort {
 
     }
 
+    public static void sort(char[] array, boolean desc) {
+
+        for (int i = 0; i < array.length - 1; i++) {
+            int index = i;
+
+            for (int j = i + 1; j < array.length; j++) {// finds the minimum element in array[i..]
+                if ((!desc && Character.compare(array[j], array[index]) < 0)
+                        || (desc && Character.compare(array[j], array[index]) > 0)) {
+                    index = j;
+                }
+            }
+            // place the minimum found in sorted part by swapping it with current array[i]
+            char minimum = array[index];
+            array[index] = array[i];
+            array[i] = minimum;
+
+        }
+
+    }
+
+    public static void sort(String[] array, boolean desc) {
+
+        for (int i = 0; i < array.length - 1; i++) {
+            int index = i;
+
+            for (int j = i + 1; j < array.length; j++) {// finds the minimum element in array[i..]
+                if ((!desc && array[j].compareTo(array[index]) < 0)
+                        || (desc && array[j].compareTo(array[index]) > 0)) {
+                    index = j;
+                }
+            }
+            // place the minimum found in sorted part by swapping it with current array[i]
+            String smallerNumber = array[index];
+            array[index] = array[i];
+            array[i] = smallerNumber;
+
+        }
+
+    }
+
     static void printArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -28,16 +68,49 @@ class SelectionSort {
         System.out.println("");
     }
 
+    static void printArray(char[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("");
+    }
+
+    static void printArray(String[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("");
+    }
+
     public static void main(String[] args) {
-        int[] array = { 1, 23, 3, 4, 2, 14, 10, 8, 20, 9, 21 };
-        System.out.println("Array before sorting");
-        printArray(array);
-        System.out.println("Array sorted in ascending order");
-        sort(array, false); // sort the array in ascending order
-        printArray(array);
-        sort(array, true); // sort the array in descending order
-        System.out.println("Array sorted in descending order");
-        printArray(array);
+        int[] numArray = { 1, 23, 3, 4, 2, 14, 10, 8, 20, 9, 21 };
+        char[] charArray = { 'a', 'z', 'K', 'A', 'X', 'M', };
+        String[] stringArray = { "zc", "aba", "ab", "ba", "abc", "a", };
+
+        System.out.println("Arrays before sorting");
+        printArray(numArray);
+        printArray(charArray);
+        printArray(stringArray);
+
+        // sorting array in Ascending order
+        sort(numArray, false);
+        sort(charArray, false);
+        sort(stringArray, false);
+
+        System.out.println("Arrays sorted in ascending order");
+        printArray(numArray);
+        printArray(charArray);
+        printArray(stringArray);
+
+        // sorting array in Descending order
+        sort(numArray, true);
+        sort(charArray, true);
+        sort(stringArray, true);
+
+        System.out.println("Arrays sorted in descending order");
+        printArray(numArray);
+        printArray(charArray);
+        printArray(stringArray);
     }
 
 }
